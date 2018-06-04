e82a99515051173c5dec16fca6fedf6736c45ad5
federicomoyamartin <federicomoyamartin@gmail.com>
2018-06-04 18:53:05 -0300

    Create develop branch

--- src/starea3/main.java
+++ src/starea3/main.java
@@ -18,6 +18,7 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // Creating develop branch
         JFrame f=new JFrame();//creating instance of JFrame  
         JButton b=new JButton("click");//creating instance of JButton  
         b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
