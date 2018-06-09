09aecf5b4bd37dabd3dae2907dbaf58097867b5b
federicomoyamartin <federicomoyamartin@gmail.com>
2018-06-08 21:54:04 -0300

    Set up login

--- src/starea3/main.java
+++ src/starea3/main.java
@@ -20,14 +20,16 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        DBConnector.initiateConnection();
-        ResultSet a = DBConnector.executeQuery("SELECT * FROM users");
-        try {
-            if (a.next()) {
-                System.out.println(a.getString(1));
-            }   } catch (SQLException ex) {
-            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//        DBConnector.initiateConnection();
+//        ResultSet a = DBConnector.executeQuery("SELECT * FROM users");
+//        try {
+//            if (a.next()) {
+//                System.out.println(a.getString(1));
+//            }   } catch (SQLException ex) {
+//            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+        new Login(null,true).show();
 
     
     
